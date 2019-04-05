dc7731e086aa308afbf3b290b420c2d2116b151e
전민균 <jun.min.gyun@gmail.com>
2019-04-05 13:05:28 +0900

    문의하기 답글 reply 완료

--- TicketZone/src/main/webapp/resources/js/reply.js
+++ TicketZone/src/main/webapp/resources/js/reply.js
@@ -1,11 +1,20 @@
 $(function() {
-	// 답글 등록시
-	$('.addReply_submit').click(function() {
+	var reply_submit_class = $('#addReply button').attr('class').trim();
+
+	if( reply_submit_class == "addReply_submit" ) {
+		$('#replyList').hide(); // 답글 없을 때
+	}else {
+		$('#addReply').hide(); // 답글 있을 때
+	}
+	
+	// 답글 등록
+	$(document).on('click', '.addReply_submit', function() {
 		var query = {
 			board_no : $('#board_no').val(),
 			reply_content : $('#addReply_content').val().trim()
 		}
 		
+		/*답글 내용 미입력시*/
 		if( query.reply_content == "") {
 			alert('답글을 입력하세요');
 			$('#addReply_content').focus();
@@ -21,12 +30,14 @@ $(function() {
 				$('#reply_head span').text(data[0].reply_reg);
 				$('#reply_content').text(data[0].reply_content);
 				$('.addReply_submit').toggleClass('addReply_submit updReply_submit');
+				$('#replyList').show();
+				$('#addReply').hide();
 			}
 		})
 	})
 	
 	//답글 수정 처리
-	$('.updReply_submit').click(function() {
+	$(document).on('click', '.updReply_submit', function() {
 		var query = {
 			reply_content : $('#addReply_content').val().trim(),
 			board_no : $('#board_no').val()
@@ -39,6 +50,8 @@ $(function() {
 			success: function(data) {
 				$('#addReply_content').val("");
 				$('#reply_content').text(query.reply_content);
+				$('#replyList').show();
+				$('#addReply').hide();
 			}
 		})
 	})
@@ -48,6 +61,8 @@ $(function() {
 //답글 수정 요청
 function reply_update_info(board_no) {
 	$('#addReply_content').val($('#reply_content').text().trim());
+	$('#replyList').hide();
+	$('#addReply').show();
 }
 
 function reply_delete(board_no) {
@@ -59,6 +74,8 @@ function reply_delete(board_no) {
 			$('#reply_head span').text("");
 			$('#reply_content').text("");
 			$('.updReply_submit').toggleClass('addReply_submit updReply_submit');
+			$('#replyList').hide();
+			$('#addReply').show();
 		}
 	})
 }
