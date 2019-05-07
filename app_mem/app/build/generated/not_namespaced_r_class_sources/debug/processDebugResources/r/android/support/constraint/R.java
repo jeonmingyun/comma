7c3e5b01de591fb011af09c9b0928fc6a524b0f0
전민균 <jun.min.gyun@gmail.com>
2019-05-07 21:25:44 +0900

    제휴매장 이미지 path 수정

--- app_mem/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app_mem/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080022;
-        public static final int end = 0x7f08003b;
-        public static final int gone = 0x7f080042;
-        public static final int invisible = 0x7f08004c;
-        public static final int left = 0x7f08004e;
-        public static final int packed = 0x7f080067;
-        public static final int parent = 0x7f080068;
-        public static final int percent = 0x7f08006a;
-        public static final int right = 0x7f080071;
-        public static final int spread = 0x7f080089;
-        public static final int spread_inside = 0x7f08008a;
-        public static final int start = 0x7f08008f;
-        public static final int top = 0x7f08009f;
-        public static final int wrap = 0x7f0800a8;
+        public static final int end = 0x7f080039;
+        public static final int gone = 0x7f080040;
+        public static final int invisible = 0x7f08004a;
+        public static final int left = 0x7f08004c;
+        public static final int packed = 0x7f080065;
+        public static final int parent = 0x7f080066;
+        public static final int percent = 0x7f080068;
+        public static final int right = 0x7f08006f;
+        public static final int spread = 0x7f080087;
+        public static final int spread_inside = 0x7f080088;
+        public static final int start = 0x7f08008d;
+        public static final int top = 0x7f08009d;
+        public static final int wrap = 0x7f0800a6;
     }
     public static final class styleable {
         private styleable() {}
--- app_mem/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/slidingpanelayout/R.java
+++ app_mem/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/slidingpanelayout/R.java
@@ -85,27 +85,27 @@ public final class R {
         public static final int actions = 0x7f080017;
         public static final int async = 0x7f08001e;
         public static final int blocking = 0x7f080021;
-        public static final int chronometer = 0x7f08002a;
-        public static final int forever = 0x7f080041;
-        public static final int icon = 0x7f080047;
-        public static final int icon_group = 0x7f080048;
-        public static final int info = 0x7f08004b;
-        public static final int italic = 0x7f08004d;
-        public static final int line1 = 0x7f08004f;
-        public static final int line3 = 0x7f080050;
-        public static final int normal = 0x7f080063;
-        public static final int notification_background = 0x7f080064;
-        public static final int notification_main_column = 0x7f080065;
-        public static final int notification_main_column_container = 0x7f080066;
-        public static final int right_icon = 0x7f080072;
-        public static final int right_side = 0x7f080073;
-        public static final int tag_transition_group = 0x7f080094;
-        public static final int tag_unhandled_key_event_manager = 0x7f080095;
-        public static final int tag_unhandled_key_listeners = 0x7f080096;
-        public static final int text = 0x7f080097;
-        public static final int text2 = 0x7f080098;
-        public static final int time = 0x7f08009b;
-        public static final int title = 0x7f08009c;
+        public static final int chronometer = 0x7f080029;
+        public static final int forever = 0x7f08003f;
+        public static final int icon = 0x7f080045;
+        public static final int icon_group = 0x7f080046;
+        public static final int info = 0x7f080049;
+        public static final int italic = 0x7f08004b;
+        public static final int line1 = 0x7f08004d;
+        public static final int line3 = 0x7f08004e;
+        public static final int normal = 0x7f080061;
+        public static final int notification_background = 0x7f080062;
+        public static final int notification_main_column = 0x7f080063;
+        public static final int notification_main_column_container = 0x7f080064;
+        public static final int right_icon = 0x7f080070;
+        public static final int right_side = 0x7f080071;
+        public static final int tag_transition_group = 0x7f080092;
+        public static final int tag_unhandled_key_event_manager = 0x7f080093;
+        public static final int tag_unhandled_key_listeners = 0x7f080094;
+        public static final int text = 0x7f080095;
+        public static final int text2 = 0x7f080096;
+        public static final int time = 0x7f080099;
+        public static final int title = 0x7f08009a;
     }
     public static final class integer {
         private integer() {}
@@ -115,12 +115,12 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0a0024;
-        public static final int notification_action_tombstone = 0x7f0a0025;
-        public static final int notification_template_custom_big = 0x7f0a0026;
-        public static final int notification_template_icon_group = 0x7f0a0027;
-        public static final int notification_template_part_chronometer = 0x7f0a0028;
-        public static final int notification_template_part_time = 0x7f0a0029;
+        public static final int notification_action = 0x7f0a0023;
+        public static final int notification_action_tombstone = 0x7f0a0024;
+        public static final int notification_template_custom_big = 0x7f0a0025;
+        public static final int notification_template_icon_group = 0x7f0a0026;
+        public static final int notification_template_part_chronometer = 0x7f0a0027;
+        public static final int notification_template_part_time = 0x7f0a0028;
     }
     public static final class string {
         private string() {}
