2f4cc1c58d84fe06c9fda3537f6015c668224095
전민균 <jun.min.gyun@gmail.com>
2019-05-08 16:49:34 +0900

    app_mem

---
+++ app_mem/app/src/main/java/com/org/ticketzone/app_mem/db/DBTable.java
@@ -0,0 +1,18 @@
+package com.org.ticketzone.app_mem.db;
+
+import android.provider.BaseColumns;
+
+public final class DBTable {
+
+    public static final class Member implements BaseColumns {
+        public static final String MEMBER_TEL = "member_tel";
+        public static final String MEMBER_NAME = "member_name";
+        public static final String MEMBER_BIRTH= "member_birth";
+        public static final String TABLENAME= "member";
+        public static final String CREATE_QUERY= "create table "+ TABLENAME + "("
+                + MEMBER_TEL + " text primary key,"
+                + MEMBER_NAME + " text not null,"
+                + MEMBER_BIRTH + " text not null );";
+        public static final String DROP_QUERY= "drop table if exists "+TABLENAME;
+    }
+}
