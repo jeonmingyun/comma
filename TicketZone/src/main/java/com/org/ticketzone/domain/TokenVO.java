d52c6fd4510b5d2b61ac1fee904e749d0f6def24
leeburim <i__pop@naver.com>
2019-06-15 14:17:17 +0900

    push 끝

--- TicketZone/src/main/java/com/org/ticketzone/domain/TokenVO.java
+++ TicketZone/src/main/java/com/org/ticketzone/domain/TokenVO.java
@@ -5,5 +5,6 @@ import lombok.Data;
 @Data
 public class TokenVO {
 	private String token_id;
-	private String member_id;	
+	private String member_id;
+	private String license_number;
 }
--- app_mngr/app/src/main/java/ticketzone/org/com/app_mngr/Task/NetworkTask.java
+++ app_mngr/app/src/main/java/ticketzone/org/com/app_mngr/Task/NetworkTask.java
@@ -18,7 +18,7 @@ public class NetworkTask extends AsyncTask<SendDataSet, Void, String> {
 
 //    private String ip = "15.164.115.73"; // 서버 IP
 
-    String ip ="39.127.7.42"; //학교 IP번호
+    String ip ="39.127.7.41"; //학교 IP번호
     //String ip = "15.164.115.73"; // 서버 IP
     String path = "http://"+ip+":8080/"; // 연결할 jsp주소
 
