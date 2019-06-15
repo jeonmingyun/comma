4da64047aaf4a553d2c9ba28f8aaac68695379ac
Moonheewon <46548594+Moonheewon@users.noreply.github.com>
2019-06-15 16:30:25 +0900

    비회원용 앱 끝

--- noneMemTicket/app/src/main/java/noneMem/ticketzone/org/com/MainActivity.java
+++ noneMemTicket/app/src/main/java/noneMem/ticketzone/org/com/MainActivity.java
@@ -1,14 +1,26 @@
 package noneMem.ticketzone.org.com;
 
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.NumberKeyListener;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+
+import static android.text.InputType.TYPE_CLASS_NUMBER;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,13 +36,13 @@ public class MainActivity extends AppCompatActivity {
     private Button del;
     private Button tel0;
     private Button send;
-    private TextView teltext;
+    private TextView teltext, textview4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textview4 = findViewById(R.id.textView4);
         tel0 = findViewById(R.id.tel0);
         tel1 = findViewById(R.id.tel1);
         tel2 = findViewById(R.id.tel2);
@@ -45,5 +57,219 @@ public class MainActivity extends AppCompatActivity {
         send = findViewById(R.id.send);
         teltext = findViewById(R.id.teltext);
 
+        tel0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel0.getText().toString(); // 0
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+
+        tel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel1.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        tel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel2.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        tel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel3.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        tel4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel4.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        tel5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel5.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        tel6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel6.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        tel7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel7.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        tel8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel8.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        tel9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = tel9.getText().toString();
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( teltext_arr.length == 1 && teltext_arr[0].length() == 3) { // tel 첫 3글자 입력
+                    teltext.append("-");
+                }else if(teltext_arr.length == 2 && teltext_arr[1].length() == 4) { // tel 중간 4글자 입력
+                    teltext.append("-");
+                }
+
+                teltext.append(txt);
+            }
+        });
+        del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String teltext_full = teltext.getText().toString();
+                String teltext_arr[] = teltext_full.split("-"); // {010} {1234} {5648}
+
+                if( !teltext_full.equals("")) {
+                    if( teltext_arr.length == 2 && teltext_arr[1].length() == 1) { // tel 첫 3글자 뒤의 -지우기
+                        teltext.setText(teltext_full.substring(0, teltext_full.length() - 2));
+                    }else if(teltext_arr.length == 3 && teltext_arr[2].length() == 1) { // tel 중간 4글자 뒤의 -지우기
+                        teltext.setText(teltext_full.substring(0, teltext_full.length() - 2));
+                    } else {
+                        teltext.setText(teltext_full.substring(0, teltext_full.length() - 1));
+                    }
+                }
+
+            }
+        });
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String teltext_full = teltext.getText().toString();
+                final EditText ET = new EditText(MainActivity.this);
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("인원 수 설정");
+                ET.setInputType(TYPE_CLASS_NUMBER);
+                dialog.setMessage("인원 수");
+                dialog.setView(ET);
+
+                if( teltext_full.length() == 13) {
+                    // 확인 버튼 이벤트
+                dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        teltext.setText("");
+                        String textTxt = textview4.getText().toString().replaceAll("팀", "");
+                        textview4.setText(Integer.parseInt(textTxt)+1 + "팀");
+                        Toast.makeText(MainActivity.this, teltext_full + "님 번호표가 발급되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                dialog.show();
+                }
+            }
+        });
+
     }
 }
+
