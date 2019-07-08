6a060bb4775d05dadeb9979ec76b3f3fea731286
uihunkim <43628581+uihunkim@users.noreply.github.com>
2019-07-08 10:46:19 +0900

    pull

--- app_mem/app/src/main/java/com/org/ticketzone/app_mem/activity/MainActivity.java
+++ app_mem/app/src/main/java/com/org/ticketzone/app_mem/activity/MainActivity.java
@@ -333,10 +333,10 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         storeList = new ArrayList<>();
         StoreVO storeVO;
         gpsTracker = new GpsTracker(MainActivity.this);
-//        my_x = gpsTracker.getLatitude();
-//        my_y = gpsTracker.getLongitude();
-          my_x =  35.89234359816278;
-          my_y = 128.6230699996115;
+        my_x = gpsTracker.getLatitude();
+        my_y = gpsTracker.getLongitude();
+//          my_x =  35.89234359816278;
+//          my_y = 128.6230699996115;
 
         while(cursor.moveToNext()) {
             storeVO = new StoreVO();
